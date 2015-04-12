31d95b709a96782085df7e0f8f57811e9a631d51
rbarrett95 <rbarrett@bowdoin.edu>
2015-04-12 11:10:02 -0400

    initial commit
    includes main.cpp

---
+++ NeuralNetworks/NeuralNetworks/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  NeuralNetworks
+//
+//  Created by Ryan Barrett on 4/12/15.
+//  Copyright (c) 2015 Ryan Barrett. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    cout << "Hello, World!\n";
+    return 0;
+}
